c46c0ff1c911ca19316e52c3f67e83f954d44415
Chinmay-Sakhare07 <sakhare.c@northeastern.edu>
2025-10-26 18:25:52 -0400

    Added all other models for student use case

---
+++ ProfileWorkareaExample_team04/src/Business/CourseWork/Submission.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.CourseWork;
+
+/**
+ *
+ * @author chinm
+ */
+import Business.Profiles.StudentProfile;
+import java.util.Date;
+
+public class Submission {
+    private StudentProfile student;
+    private Assignment assignment;
+    private Date submissionDate;
+    private int grade;
+}
