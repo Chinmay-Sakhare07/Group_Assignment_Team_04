da89e17f7db1f73c5d8aa93c9fd99afbbce595b0
Chinmay-Sakhare07 <sakhare.c@northeastern.edu>
2025-10-26 22:14:48 -0400

    Committing models and logic for the design

--- ProfileWorkareaExample_team04/src/Business/Profiles/StudentAccount.java
+++ ProfileWorkareaExample_team04/src/Business/Profiles/StudentAccount.java
@@ -9,6 +9,46 @@ package Business.Profiles;
  *
  * @author kal bugrara
  */
+import Business.Finance.Payment;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class StudentAccount {
-    
+
+    private double balance;
+    private ArrayList<Payment> paymentHistory;
+
+    public StudentAccount() {
+        this.balance = 0.0;
+        this.paymentHistory = new ArrayList<>();
+    }
+
+    public void billTuition(double amount) {
+        balance += amount;
+        paymentHistory.add(new Payment(amount, "Tuition", "Billed", new Date()));
+    }
+
+    public void makePayment(double amount) {
+        balance -= amount;
+        paymentHistory.add(new Payment(amount, "Payment", "Paid", new Date()));
+    }
+
+    public void refund(double amount) {
+        balance -= amount;
+        paymentHistory.add(new Payment(amount, "Refund", "Refunded", new Date()));
+    }
+
+    public double getOutstandingBalance() {
+        return balance;
+    }
+
+    public ArrayList<Payment> getPaymentHistory() {
+        return paymentHistory;
+    }
+
+    @Override
+    public String toString() {
+        return "Balance: $" + balance;
+    }
+
 }
